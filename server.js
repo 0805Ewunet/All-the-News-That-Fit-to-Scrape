7ec80e799b3a658cd7c47fce785027ac96e32d75
Ewunet Kassa <ewunetk@yahoo.com>
2019-06-17 12:45:31 -0400

    working functionality

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express    = require('express'),
+
     bodyParser = require('body-parser'),
+
     exphbs     = require('express-handlebars'),
+
     logger     = require("morgan");
 
 var PORT = process.env.PORT || 3000;
@@ -10,27 +13,40 @@ var app = express();
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.use(express.static("public"));
 
-
 app.set('views', './views')
+
 app.engine(
+
   "handlebars",
+
   exphbs({
+
     defaultLayout: "main"
+
   })
+
 );
+
 app.set("view engine", "handlebars");
+
 
 require('./routes/apiRoutes')(app)
 
-
 app.listen(PORT, function() {
+
     console.log(
+
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+
       PORT,
+
       PORT
+
     );
+
   });
